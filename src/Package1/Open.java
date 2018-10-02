1e9e3b34dec77c04bdee59d6ecd0bd8a01f4ae60
kartik <kartiksaxena500@gmail.com>
2018-10-02 12:09:13 +0530

    Addedfiles of the project

---
+++ src/Package1/Open.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Package1;
+
+/**
+ *
+ * @author Samsung
+ */
+public class Open {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Login_page ob=new Login_page();
+     ob.setVisible(true);// TODO code application logic here
+    }
+    
+}
